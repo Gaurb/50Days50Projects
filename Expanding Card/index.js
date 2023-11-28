942ae0dd7fb769eac2c8d8231381c099566449ba
Gaurb <89831065+Gaurb@users.noreply.github.com>
2023-11-28 08:41:22 +0530

    initial commit

---
+++ Expanding Card/index.js
@@ -0,0 +1,14 @@
+var panels=document.querySelectorAll(".panel");
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click',()=>{
+        removeClass();
+        panel.classList.add('active');
+    });
+});
+
+function removeClass(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active');
+    })
+}
